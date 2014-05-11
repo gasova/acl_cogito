b7b521b457f701e81a2b1cbbd9f06f2290e4a152
Gökçe ASOVA <gokce@asova.com>
2014-05-11 21:02:59 +0200

    .
    .

--- lib/acl_cogito/controllers/extensions.rb
+++ lib/acl_cogito/controllers/extensions.rb
@@ -6,13 +6,13 @@ module AclCogito
         base.extend ClassMethods
         base.send :include, InstanceMethods
 
-        base.class_eval do
-          (class << self; self; end).instance_eval do
-            define_method "#{AclCogito.model_name.underscore}_destroy_conditions" do |&block|
-              AclCogito.set_destroy_conditions( &block )
-            end
-          end
-        end
+        #base.class_eval do
+        #  (class << self; self; end).instance_eval do
+        #    define_method "#{AclCogito.model_name.underscore}_destroy_conditions" do |&block|
+        #      AclCogito.set_destroy_conditions( &block )
+        #    end
+        #  end
+        #end
       end
 
       module ClassMethods
@@ -22,9 +22,9 @@ module AclCogito
       end
 
       module InstanceMethods
-        def can_destroy_acl_cogito?(opinio)
-          self.instance_exec(opinio, &AclCogito.destroy_conditions)
-        end
+        #def can_destroy_acl_cogito?(opinio)
+        #  self.instance_exec(opinio, &AclCogito.destroy_conditions)
+        #end
 
         def acl_cogito_after_create_path(resource)
           resource.is_a?(AclCogito.model_name.constantize) ? resource.commentable : resource
