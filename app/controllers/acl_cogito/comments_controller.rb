49487b7dbe4db88b092d02df46ae2477b0d0b423
Gökçe ASOVA <gokce@asova.com>
2014-05-11 20:09:49 +0200

    .
    .

--- app/controllers/acl_cogito/comments_controller.rb
+++ app/controllers/acl_cogito/comments_controller.rb
@@ -47,7 +47,7 @@ class AclCogito::CommentsController < ApplicationController
   private
   
   def comment_params
-    params.permit comment: [:body, :owner_id, :commentable_id, :commentable_type]
+    params.require(:comment).permit(:owner_id, :commentable_id, :commentable_type, :comment => [:body])
   end
   
 end
